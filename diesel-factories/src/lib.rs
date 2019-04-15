490df9d6727eeeaf787a05089f34b689832dbbdf
David Pedersen <david.pdrsn@gmail.com>
2019-04-15 15:29:08 +0200

    Add docs for `#[derive(Factory)]`

--- diesel-factories/src/lib.rs
+++ diesel-factories/src/lib.rs
@@ -1,3 +1,14 @@
+//! This libraries makes it straight forward to create [factories][] that work with [Diesel][].
+//!
+//! [factories]: https://thoughtbot.com/blog/why-factories
+//! [Diesel]: https://dielse.rs
+//!
+//! # WIP
+//!
+//! It is still very much work in progress so expect breaking changes at any point.
+//!
+//! # Example usage
+//!
 //! ```
 //! #[macro_use]
 //! extern crate diesel;
@@ -65,6 +76,127 @@
 //!     assert_eq!(30, user_from_db.age);
 //! }
 //! ```
+//!
+//! # What `#[derive(Factory)]` does
+//!
+//! Deriving `Factory` on a struct will do the following:
+//!
+//! 1. Implement [`DefaultFactory`][].
+//! 1. Implement [`InsertFactory`][].
+//! 1. Add methods to change the values set by your [`Default`][] implementation.
+//!
+//! For example this:
+//!
+//! ```
+//! # #[macro_use]
+//! # extern crate diesel;
+//! # use diesel::prelude::*;
+//! # use diesel::pg::PgConnection;
+//! # use diesel_factories::{Factory, InsertFactory, DefaultFactory};
+//! # table! {
+//! #     users (id) {
+//! #         id -> Integer,
+//! #         name -> Text,
+//! #         age -> Integer,
+//! #     }
+//! # }
+//! # fn main() {}
+//! # #[derive(Queryable, Identifiable)]
+//! # pub struct User {
+//! #     pub id: i32,
+//! #     pub name: String,
+//! #     pub age: i32,
+//! # }
+//! # impl Default for UserFactory {
+//! #     fn default() -> UserFactory {
+//! #         UserFactory {
+//! #             name: "Bob".into(),
+//! #             age: 30,
+//! #         }
+//! #     }
+//! # }
+//! #
+//! #[derive(Insertable, Factory)]
+//! #[table_name = "users"]
+//! #[factory_model(User)]
+//! pub struct UserFactory {
+//!     name: String,
+//!     age: i32,
+//! }
+//! ```
+//!
+//! Expands into this:
+//!
+//! ```
+//! # #[macro_use]
+//! # extern crate diesel;
+//! # use diesel::prelude::*;
+//! # use diesel::pg::PgConnection;
+//! # use diesel_factories::{Factory, InsertFactory, DefaultFactory};
+//! # table! {
+//! #     users (id) {
+//! #         id -> Integer,
+//! #         name -> Text,
+//! #         age -> Integer,
+//! #     }
+//! # }
+//! # fn main() {}
+//! # #[derive(Queryable, Identifiable)]
+//! # pub struct User {
+//! #     pub id: i32,
+//! #     pub name: String,
+//! #     pub age: i32,
+//! # }
+//! # impl Default for UserFactory {
+//! #     fn default() -> UserFactory {
+//! #         UserFactory {
+//! #             name: "Bob".into(),
+//! #             age: 30,
+//! #         }
+//! #     }
+//! # }
+//! #
+//! #[derive(Insertable)]
+//! #[table_name = "users"]
+//! pub struct UserFactory {
+//!     name: String,
+//!     age: i32,
+//! }
+//!
+//! impl DefaultFactory<UserFactory> for User {}
+//!
+//! impl InsertFactory<User> for UserFactory {
+//!     fn insert<Con>(self, con: &Con) -> User
+//!     where
+//!         Con: diesel::connection::Connection<Backend = diesel::pg::Pg>,
+//!     {
+//!         let res = diesel::insert_into(<User as diesel::associations::HasTable>::table())
+//!             .values(self)
+//!             .get_result::<User>(con);
+//! 
+//!         match res {
+//!             Ok(inner) => inner,
+//!             Err(err) => panic!("{}", err),
+//!         }
+//!     }
+//! }
+//!
+//! impl UserFactory {
+//!     pub fn name<T: Into<String>>(mut self, value: T) -> Self {
+//!         self.name = value.into();
+//!         self
+//!     }
+//!
+//!     pub fn age<T: Into<i32>>(mut self, value: T) -> Self {
+//!         self.age = value.into();
+//!         self
+//!     }
+//! }
+//! ```
+//!
+//! [`DefaultFactory`]: trait.DefaultFactory.html
+//! [`InsertFactory`]: trait.InsertFactory.html
+//! [`Default`]: https://doc.rust-lang.org/std/default/trait.Default.html
 
 use diesel::backend::Backend;
 use diesel::backend::SupportsDefaultKeyword;
